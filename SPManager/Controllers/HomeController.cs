f2e1005bd7ffe090fb7d454ef27b3d20ac76e0b3
blad90 <bbaez_11@hotmail.com>
2018-03-09 15:03:57 -0400

    Added default university image

--- SPManager/Controllers/HomeController.cs
+++ SPManager/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SPManager.Models;
+using SPManager.Views;
 
 namespace SPManager.Controllers
 {
     public class HomeController : Controller
     {
+        private SPDBEntities db = new SPDBEntities();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,15 @@ namespace SPManager.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<StatsGroup> data = from university in db.Universities
+                                                   group university by university.Name into statsGroup
+                                                   select new StatsGroup()
+                                                   {
+                                                       UniversityName = statsGroup.Key,
+                                                       UniversityCount = statsGroup.Count()
+                                                   };
 
-            return View();
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
---
+++ SPManager/Views/StatsGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPManager.Views
+{
+    public class StatsGroup
+    {
+        public string UniversityName { get; set; }
+        public int UniversityCount { get; set; }
+    }
+}
